56027782de726c94073132e896e955295613ab1e
Caner Uguz <caneruguz@gmail.com>
2014-07-24 16:58:59 -0400

    Starting grid build, some commenting

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,46 @@
 (function(){
 var totalWidth = 0; 
-var modules = [];
+var modules = []; // created on load and checked at every resize
 var dragState =  false;
+
 
 var reformat = function(){
     // Size wrapper elements
-    var headfinal = $(window).width()-120;
+    var headfinal = $(window).width()-120; // final width of the header taking into account the navbar
     $('#ht-head').css({ width : headfinal + 'px' } ); 
     $('#ht-wrapper').css({ width : headfinal + 'px' } ); 
 
+    // Resize header modules 
     for(var i = 0; i < modules.length; i++){
         var o = modules[i]; 
-        var contentWidth = $('#ht-content').width(); 
-        var headWidth = $('#ht-head').width(); 
+        var contentWidth = $('#ht-content').width();  // width of the module
+        var headWidth = $('#ht-head').width();  
         var width = o.width/contentWidth*100;
-        $('.ht-hdiv[data-hid="'+o.id+'"]').css( { width : width+'%'});
-        
+        $('.ht-hdiv[data-hid="'+o.id+'"]').css( { width : width+'%'});    
     }
 
+    // Trying out the grid, unfinished
+    var windowH = $(window).height();
+    var wrapHeight = $('#ht-wrapper').height(windowH-120);  
+
+    var totalRow = $('.h-row').length; 
+    $('.h-row').each(function(){
+        $(this).height(wrapHeight/3);
+    })
 }
 
-
+// Build the header modules from the modules loaded 
 $('.ht-tab').each(function(){
     var header = $(this).children('.ht-tab-header');
-    var id = $(this).attr('data-id');
-    var title = header.children("h2").text();
-    var width = $(this).width(); 
+    var id = $(this).attr('data-id'); // Get id 
+    var title = header.children("h2").text(); // Get the title 
+    var width = $(this).width();  // Get the width 
     totalWidth = totalWidth+width;
-    var bg = header.attr("data-bg"); 
+    var bg = header.attr("data-bg");
+    // Build the head 
     $('#ht-head').append('<div class="ht-hdiv bg-'+bg+'" data-hid="'+id+'" >'+title+'</div>')
 
+    // ScrollTo initialization. 
     $(document).on('click', '.ht-hdiv', function(){
         var id = $(this).attr('data-hid');
         $('#ht-wrapper').scrollTo($('.ht-tab[data-id="'+id+'"]'), 200,  {offset:-50});
@@ -37,6 +48,8 @@ $('.ht-tab').each(function(){
     modules.push({id : id, width : width});
     reformat();  
 })
+
+// jquery-ui Resizable options
 $('.ht-tab').resizable({
     grid: 50,
     minWidth : 300,
